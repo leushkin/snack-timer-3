ac47107ae62ed7aaf2af8cfdd7e5d6b8cd3a07a7
leushkin <kirill.leushkin1@gmail.com>
2024-06-02 22:31:12 +0300

    Initial Commit

---
+++ snack-timer-3 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snack-timer-3 Watch App
+//
+//  Created by Work on 02.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snack-timer-3 Watch App/snack_timer_3App.swift
@@ -0,0 +1,17 @@
+//
+//  snack_timer_3App.swift
+//  snack-timer-3 Watch App
+//
+//  Created by Work on 02.06.24.
+//
+
+import SwiftUI
+
+@main
+struct snack_timer_3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
